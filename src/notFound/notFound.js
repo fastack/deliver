8b780a4a432c5bbbdca19ce92fd77a162f54cf75
Patrick DeVivo <patrick.devivo@gmail.com>
2015-08-23 13:18:02 -0400

    updates

--- src/notFound/notFound.js
+++ src/notFound/notFound.js
@@ -2,7 +2,7 @@ module.exports = function() {
     return function(req, res, next) {
        // TODO: allow 404s to be configurable
        // return a specific file or redirect somewhere
-       res.writeHead(404);
-       res.end('Page not found...');
+        res.statusCode = 404;
+        res.end('Page not found...');
     }
 };
